bb34594cc751dc9df2cff85b586fcdcba04898a4
https://github.com/somykm <sk43@hood.edu>
2025-09-10 23:40:39 -0400

    gateway application has been set up and runing successfully, and return the patients

--- src/main/java/com/abernathyclinic/medilabo_gateway/MedilaboGatewayApplication.java
+++ src/main/java/com/abernathyclinic/medilabo_gateway/MedilaboGatewayApplication.java
@@ -9,22 +9,31 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MedilaboGatewayApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(MedilaboGatewayApplication.class, args);
-	}
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
+        SpringApplication.run(MedilaboGatewayApplication.class, args);
+    }
 
-				.route("patient-service", r -> r
-						.path("/api/patient/**")
-						.filters(f -> f
-								.addRequestHeader("X-Gateway-Source", "MedilaboGateway")
-								.circuitBreaker(config -> config
-										.setName("patientCircuitBreaker")
-										.setFallbackUri("forward:/fallback")))
-						.uri("http://localhost:8081"))
-				.build();
-	}
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                .route("patient-service", r -> r
+                        .path("/api/patient/**")
+
+                        .filters(f -> f
+                                .addRequestHeader("X-Gateway-Source", "MedilaboGateway")
+                                .circuitBreaker(config -> config
+                                        .setName("patientCircuitBreaker")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri("http://localhost:8081"))
+                // Route to frontend service
+                .route("frontend-service", r -> r
+                        .path("/ui/**")
+                        .uri("http://localhost:8082"))
+                .route("test-route", r -> r
+                        .path("/test/**")
+                        .uri("http://localhost:8081"))
+                .build();
+    }
 }
