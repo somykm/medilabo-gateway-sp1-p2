7efe03fe99884b5321ac44a4f4ca6fbe8df9fa3b
https://github.com/somykm <sk43@hood.edu>
2025-12-04 03:14:54 -0500

    To check the logic of the app temporarily commend out the security completely
    Signed-off-by: https://github.com/somykm <sk43@hood.edu>

--- src/main/java/com/abernathyclinic/medilabo_gateway/controller/FallbackController.java
+++ src/main/java/com/abernathyclinic/medilabo_gateway/controller/FallbackController.java
@@ -1,17 +1,17 @@
-package com.abernathyclinic.medilabo_gateway.controller;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-public class FallbackController {
-
-    @GetMapping("/fallback")
-    public ResponseEntity<String> fallback() {
-        log.warn("Fallback triggered: Patient service is currently unavailable.");
-        return ResponseEntity.ok("Fallback response: Patient service is currently unavailable.");
-    }
-}
+//package com.abernathyclinic.medilabo_gateway.controller;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//public class FallbackController {
+//
+//    @GetMapping("/fallback")
+//    public ResponseEntity<String> fallback() {
+//        log.warn("Fallback triggered: Patient service is currently unavailable.");
+//        return ResponseEntity.ok("Fallback response: Patient service is currently unavailable.");
+//    }
+//}
